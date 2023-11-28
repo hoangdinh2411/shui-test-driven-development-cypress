f0d4698a97d56292313e129fb8737ac871e0336b
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-11-28 23:33:58 +0100

    update ci

--- cypress/e2e/messages.cy.js
+++ cypress/e2e/messages.cy.js
@@ -1,15 +1,18 @@
 describe('template spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/');
-    cy.wait(2000);
   });
 
   it('display all messages that are stored in local storage', () => {
+    cy.wait(2000);
+
     cy.get('.messages').should('be.visible');
     cy.get('.messages').children().should('have.length.greaterThan', 1);
   });
 
   it('display the message info', () => {
+    cy.wait(2000);
+
     cy.get('.author').first().should('have.text', 'John');
     cy.get('.content').first().should('have.text', 'Hello world');
   });
