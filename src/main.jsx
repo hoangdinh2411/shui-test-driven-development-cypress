1200dc2117c6a496533fb56839816c9da854a701
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-11-29 13:08:15 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,21 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Messages from './pages/Messages.jsx';
 import Post from './pages/Post.jsx';
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Messages />,
+    },
+    {
+      path: '/post',
+      element: <Post />,
+    },
+  ],
   {
-    path: '/',
-    element: <Messages />,
-  },
-  {
-    path: '/post',
-    element: <Post />,
-  },
-]);
+    basename: '/',
+  }
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
