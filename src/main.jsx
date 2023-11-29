8459c08fee509f2dcb4dc36c16aef805816fde08
William Hoang Dinh <hoangdinh2411@gmail.com>
2023-11-29 14:41:59 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,22 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Messages from './pages/Messages.jsx';
 import Post from './pages/Post.jsx';
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      index: true,
+      path: '/',
+      element: <Messages />,
+    },
+    {
+      path: '/post',
+      element: <Post />,
+    },
+  ],
   {
-    path: '/',
-    element: <Messages />,
-  },
-  {
-    path: '/post',
-    element: <Post />,
-  },
-]);
+    base: '/shui-test-driven-development-cypress',
+  }
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
